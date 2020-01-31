6aa08b37785b1c466cc4c65cf2aa8cd216d4a13b
Leif Van Holland <holland@uni-bonn.de>
2020-01-31 15:12:14 +0100

    correct path for docstring coverage

--- docstrcoverage.py
+++ docstrcoverage.py
@@ -3,7 +3,7 @@ import os
 
 # collect files
 files = []
-for dirpath, _, filenames in os.walk('.'):
+for dirpath, _, filenames in os.walk('training_scheduler'):
     files.extend(os.path.join(dirpath, name) for name in filenames if name.endswith('.py'))
 
 get_docstring_coverage(files, verbose=3)
