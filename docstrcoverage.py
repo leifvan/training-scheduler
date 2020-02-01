a409f3cf06d6eda773e5d6787ed3cacda1bf7121
Leif Van Holland <holland@uni-bonn.de>
2020-02-01 11:39:07 +0100

    move code into main

--- docstrcoverage.py
+++ docstrcoverage.py
@@ -2,9 +2,10 @@ from docstr_coverage import get_docstring_coverage
 from typing import List, Union
 import os
 
-# collect files
-files: List[Union[os.PathLike, str]] = []
-for dirpath, _, filenames in os.walk('training_scheduler'):
-    files.extend(os.path.join(dirpath, name) for name in filenames if name.endswith('.py'))
+if __name__ == "__main__":
+    # collect files
+    files: List[Union[os.PathLike, str]] = []
+    for dirpath, _, filenames in os.walk('training_scheduler'):
+        files.extend(os.path.join(dirpath, name) for name in filenames if name.endswith('.py'))
 
-get_docstring_coverage(files, verbose=3)
+    get_docstring_coverage(files, verbose=3)
