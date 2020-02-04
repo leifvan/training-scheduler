069ea99b513eb830aa2586b9a81a7f7b3b2ad31e
Leif Van Holland <leifvan.holland@gmx.de>
2020-02-04 13:34:43 +0100

    include examples subpackage in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='training-scheduler',
     version='0.0.2',
-    packages=['training_scheduler'],
+    packages=['training_scheduler', 'training_scheduler.examples'],
     url='',
     license='Unlicense',
     author='Leif Van Holland',
